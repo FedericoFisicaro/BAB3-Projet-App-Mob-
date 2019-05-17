135dea0015268ed3255800acc1a7d35da0a9cf1f
Martin <martin.maes@student.umons.ac.be>
2019-05-17 17:36:16 +0200

    2ème version design

--- app/src/main/java/be/federico/projetappmob/DetectActivity.java
+++ app/src/main/java/be/federico/projetappmob/DetectActivity.java
@@ -25,7 +25,7 @@ public class DetectActivity extends AppCompatActivity {
         photo=findViewById(R.id.ImageView);
 
 
-        
+
 
         prendrePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/be/federico/projetappmob/InfoActivity.java
+++ app/src/main/java/be/federico/projetappmob/InfoActivity.java
@@ -2,12 +2,18 @@ package be.federico.projetappmob;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class InfoActivity extends AppCompatActivity {
 
     ListView lv;
-
+    ArrayList<String> noms;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +21,20 @@ public class InfoActivity extends AppCompatActivity {
 
         lv=findViewById(R.id.listView);
 
+        noms=new ArrayList<String>();
+        noms.add("papier");
+        noms.add("carton");
+        noms.add("PMC");
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.activity_info,noms);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView txt=(TextView)view;
+                //System.out
+            }
+        });
 
     }
 }
