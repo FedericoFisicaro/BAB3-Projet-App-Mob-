563cb742d0052b639fd2521c50b0d0f8dcdc37db
Martin <martin.maes@student.umons.ac.be>
2019-05-17 16:27:46 +0200

    2ème version design

--- app/src/main/java/be/federico/projetappmob/AideActivity.java
+++ app/src/main/java/be/federico/projetappmob/AideActivity.java
@@ -1,13 +1,24 @@
 package be.federico.projetappmob;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AideActivity extends AppCompatActivity {
-
+    Button retour;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aide);
+        retour=findViewById(R.id.bRetour);
+        retour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(AideActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
