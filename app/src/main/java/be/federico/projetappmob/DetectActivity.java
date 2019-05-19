667fde55f1cf129a5caba99ca9944885fec281e3
federico <171538@umons.ac.be>
2019-05-20 01:07:36 +0200

    photo s'enregistre mais s'affiche pas (ça crash quand on reviens apres avoir fait la photo : java.io.filenotfoundexception (permission denied))

--- app/src/main/java/be/federico/projetappmob/DetectActivity.java
+++ app/src/main/java/be/federico/projetappmob/DetectActivity.java
@@ -2,6 +2,10 @@ package be.federico.projetappmob;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -9,11 +13,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DetectActivity extends AppCompatActivity {
 
     Button prendrePhoto,detect;
     ImageView photo;
+    Uri pictureUri;
 
 
     @Override
@@ -31,6 +43,18 @@ public class DetectActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                //ces 2 lignes de code jsp ce que ça fait mais sans ça ça marche pas
+                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                StrictMode.setVmPolicy(builder.build());
+
+                File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES); //prend la reference du fichier ou on stock les ficher comme musique images, videos etc
+                String pictureName =  getPictureName();
+                File imageFile = new File(pictureDirectory, pictureName);
+                pictureUri = Uri.fromFile(imageFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);  // 1er argument = je veux l'enregistrer 2eme= où l'enregistrer (soit public pour que qutres appli peuvent l'utiliser )
+
+
                 startActivityForResult(intent,0);
             }
         });
@@ -45,11 +69,30 @@ public class DetectActivity extends AppCompatActivity {
     }
 
 
+    private String getPictureName(){
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        String timestamp = sdf.format(new Date());
+        return "SmartBin" + timestamp + ".jpg";
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap=(Bitmap)data.getExtras().get("data");
-        photo.setImageBitmap(bitmap);
+
+        InputStream inputStream;
+
+        try {
+            inputStream = getContentResolver().openInputStream(pictureUri);
+            Bitmap image = BitmapFactory.decodeStream(inputStream);
+            photo.setImageBitmap(image);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Impossible ouvrir l'image", Toast.LENGTH_LONG).show();
+        }
+        //Bitmap bitmap=(Bitmap)data.getExtras().get("data");
+        //photo.setImageBitmap(bitmap);
     }
 
 }
