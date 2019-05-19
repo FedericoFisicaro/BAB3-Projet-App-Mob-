94e9fa8829e4b1f0eabcbff8bf220e3c555aa854
Martin <martin.maes@student.umons.ac.be>
2019-05-19 17:54:15 +0200

    2ème version design

---
+++ app/src/main/java/be/federico/projetappmob/DataBase.java
@@ -0,0 +1,23 @@
+package be.federico.projetappmob;
+
+import android.content.Context;
+import android.database.sqlite.*;
+
+public class DataBase {
+}
+
+
+/*
+
+public class DatabaseOpenHelper extends SQLiteAssetHelper {
+    //private static final String DATABASE_NAME="MyExternalDataBase.db";
+    private static final String DATABASE_NAME="BddDeTest.db";
+    private static final int DATABASE_VERSION=14;
+
+    //Constructor
+
+    public DatabaseOpenHelper(Context context){
+        super(context, DATABASE_NAME, null,DATABASE_VERSION);
+        setForcedUpgrade(); //pour mettre à jour la bdd dans l'emulateur quand on change la version du database
+    }
+}*/
